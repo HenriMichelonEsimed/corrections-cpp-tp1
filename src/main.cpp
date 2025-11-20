f671ffb93d50b4a6e00ab68e318db3bcf32a2bac
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2025-11-20 09:33:54 +0100

    Update 11/2025

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,12 @@ import exo9;
 import lzw;
 
 int main(int argc, char **argv) {
-    // exo1::Application app;
-    // exo9::Application app;
-    if (argc != 4) {
-        std::cerr << "Not enough arguments" << std::endl;
-        return 1;
-    }
-    lzw::Application app(argv[1], argv[2], argv[3]);
+    exo1::Application app;
+    //exo9::Application app;
+    // if (argc != 4) {
+    //     std::cerr << "Not enough arguments" << std::endl;
+    //     return 1;
+    // }
+    //lzw::Application app(argv[1], argv[2], argv[3]);
     return 0;
 }
